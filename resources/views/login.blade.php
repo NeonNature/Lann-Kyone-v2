7eff0474d228af00337ecdb4dc94c8b90016b9a3
archonn <waingwaing8@gmail.com>
2018-07-28 13:34:01 +0630

    register+login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Mentee Login</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<title>Lan Kyone</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div class="container">
 	@if (\Session::has('errors'))
       <div class="alert alert-danger">
          <p>{{ \Session::get('errors') }}</p>
@@ -17,23 +21,32 @@
          <p>{{ \Session::get('status') }}</p>
       </div><br />
       @endif
+  <img src="images/logo.png" alt="Logo" style="margin:0 auto; display: table;" class="img-fluid" />
+  <br /><br />
+  <form class="form-horizontal" action={{url('user/login')}} method="POST">
+  	{{csrf_field()}}
+    <div class="form-group">
+      <div class="col-sm-12">
+        <input type="tel" class="form-control" id="phone" placeholder="Phone Number" name="phone">
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-sm-12">          
+        <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+      </div>
+    </div>
 
-		<form action={{url('user/login')}} method="POST">
-			<table>
-				{{csrf_field()}}
-				<tr>
-					<td><label>Phone: </label></td>
-					<td><input type="text" name="phone"></td>
-				</tr>
-				<tr>
-					<td><label>Password: </label></td>
-					<td><input type="password" name="password"></td>
-				</tr>
-				<tr>
-					<td><input type="submit" name="btnSubmit" value="Login"></td>
-				</tr>
-			</table>
-		</form>
-	</div>
+    <div class="form-group">        
+      <div class="col-sm-12">
+        <button type="submit" class="btn btn-block btn-outline-success">Log In</button>
+      </div>
+    </div>
+  </form>
+  <p class="text-center">
+    Don't have an account? <a href={{url('user/create')}}>Register here!</a>
+  </p>
+</div>
+
+
 </body>
 </html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,12 +1,62 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Mentee registration</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Register - Lan Kyone</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  
+<script>
+   function checkForm(form)
+  {
+    if(form.name.value == "") {
+      alert("Error: You have a name, right?");
+      form.name.focus();
+      return false;
+    }
+    sn = /^[a-zA-Z\s]+$/;
+    re = /^\w+$/;
+    if(!sn.test(form.name.value)) {
+      alert("Error: Your name seems weird. Use only letters and spaces please.");
+      form.name.focus();
+      return false;
+    }
+    if(form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
+      if(form.pwd1.value.length < 6) {
+        alert("Error: Please make a password greater than 6 characters!");
+        form.pwd1.focus();
+        return false;
+      }
+      if(form.pwd1.value == form.name.value) {
+        alert("Error: You shouldn't use your name as your password!");
+        form.pwd1.focus();
+        return false;
+      }
+      re = /[0-9]/;
+      if(!re.test(form.pwd1.value)) {
+        alert("Error: Please put at least one number in your password!");
+        form.pwd1.focus();
+        return false;
+      }
+    } else {
+      alert("Error: We sense that your passwords don't match!");
+      form.pwd1.focus();
+      return false;
+    }
+    return true;
+  }
+</script>
+
 </head>
 <body>
-	<div class="container"> 
-	@if ($errors->any())
+
+  <div class="container">
+  @if ($errors->any())
       <div class="alert alert-danger">
           <ul>
               @foreach ($errors->all() as $error)
@@ -19,40 +69,31 @@
       <div class="alert alert-success">
           <p>{{ \Session::get('success') }}</p>
       </div><br />
-      @endif
-	<form action={{url('user')}} method="POST">
-		<table>
-			<input type="text" name="_token" value={{csrf_token()}}>
-			<tr>
-				<td><label>Full Name : </label></td>
-				<td><input type="text" name="name"></td>
-			</tr>
-			<tr>
-				<td><label>Phone : </label></td>
-				<td><input type="text" name="phone"></td>
-			</tr>
-			<tr>
-				<td><label>Password : </label></td>
-				<td><input type="password" name="password"></td>
-			</tr>
-			<tr>
-				<td><label>Confirm Password : </label></td>
-				<td><input type="password" name="confirmPassword"></td>
-			</tr>
-			<tr>
-				<td><label>Organization : </label></td>
-				<td><input type="text" name="organization"></td>
-			</tr>
-			<tr>
-				<td><label>Gender : </label></td>
-				<td><input type="radio" name="gender" value="1">Male
-					<input type="radio" name="gender" value="0">Female</td>
-			</tr>
-			<tr>
-				<td><input type="submit" name="btnSubmit" value="Register"></td>
-			</tr>
-		</table>
-	</form>
-</div>
+      @endif 
+  <br />
+  <h2 style="display: table; margin:0 auto;" class="text-info">Register</h2>
+  <hr />
+    <br />
+ 
+    <form class="form-horizontal" name="register" action={{url('user')}} method="POST" onsubmit="return checkForm(this);">
+      <input type="hidden" name="_token" value={{csrf_token()}}>
+          <input type="text" class="form-control mt-2" id="name" placeholder="Full name" name="name" required />
+          <input type="text" class="form-control mt-2" id="org" placeholder="Organization" name="organization" required />
+      <label class="radio-inline  mt-2" style="margin-right:70px;">
+      <input type="radio" name="gender" value="1"> Male
+    </label>
+    <label class="radio-inline  mt-2">
+      <input type="radio" name="gender" value="0"> Female
+    </label>
+          <input type="tel" class="form-control mt-2" id="phone" placeholder="Phone number" name="phone" required />
+          <input type="password" class="form-control mt-2" id="pwd1" placeholder="Password" name="pwd1" required />
+          <input type="password" class="form-control mt-2" id="pwd2" placeholder="Confirm password" name="pwd2" required />
+          <button type="submit" class="btn shadow btn-outline-success btn-block mt-2" name="ssubmit">Register</button>
+        </div>
+      </div>
+    </form>
+  </div>
+<hr />
 </body>
+
 </html>
